5395ba45e479a9bd153e5b14ae517bd4b11d4c91
ericoborgonove <borgonove@gmail.com>
2023-09-21 18:53:05 -0400

    aula do dia 21/09

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const routes = require ('./routes/routes')
 const app = express ()
-const port = 3000
+require ('dotenv').config()
 
 app.use ('/', routes)
 
-app.listen (port, () =>{
-    console.log (`App rodando na porta ${port}`)
+app.listen (process.env.PORT, () =>{
+    console.log (`App rodando na porta ${process.env.PORT}`)
 })
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,22 @@
 const express = require('express')
-const router = express.Router()
+const { engine } = require('express-handlebars')
+const router = express()
+//const router1 = express()
 const path = require('path')
+//router.use (express.static('./public/'))
 
+router.engine('handlebars', engine())
+router.set ('view engine', 'handlebars')
+
+router.get ('/h1', (req, res) =>{
+    res.render('index')
+})
+router.get ('/pagina1', (req, res) =>{
+    res.render('pagina1')
+})
+router.get ('/pagina1', (req, res) => {
+    res.sendFile(path.resolve('.public/pagina1.html'))
+})
 router.get ('/', (req, res) => {
     res.send ('Olá Mundo')
 })
@@ -11,6 +26,7 @@ router.get ('/home', (req, res) => {
 router.get ('/teste', (req, res) =>{
     res.send ('teste')
 })
+
 router.use(function (req,res, next){
     res.status(404).send ('Rota não encontrada')
 })
